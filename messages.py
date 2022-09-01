bef3d73eac1f068fcf6fdf6392329e98820c806d
Rittwick Bhabak <mcs222054@cse.iitd.ac.in>
2022-09-01 15:59:24 +0530

    docstrings added to state.py and messages.py modules

--- messages.py
+++ messages.py
@@ -1,6 +1,21 @@
+'''This module has the ability of showing messages and asking yes/no questions.'''
+
+
 from tkinter import messagebox as tmsg 
 
 def show_message(status, message):
+    '''Shows messages to user.
+    
+    Arguments:
+        status: string either 'error' or 'warning' or 'info'
+
+        message: a string containing the message to show
+    
+    Returns: 
+        None 
+
+    '''
+
     if status=='error':
         tmsg.showerror('Error', message)
     elif status=='warning':
@@ -9,5 +24,15 @@ def show_message(status, message):
         tmsg.showinfo('Message', message)
 
 def askquestion(status, message):
+    '''Asks yes/no question to the user. 
+    
+    Arguments:
+        message: a string containing the question to ask.
+    
+    Returns:
+        None 
+
+    '''
+    
     value = tmsg.askquestion(status, message)
     return value 
--- state.py
+++ state.py
@@ -1,14 +1,56 @@
+'''This modules holds the state of the screen.
+
+All of the screen has to be registered to the State class object.
+
+'''
+
+
 from tkinter import * 
 
 class State():
+    '''All of the screen has to registered to the state class object.
+    
+    All of the showing and hiding of the screens will be happened through this\
+        state object.
+
+    Attributes:
+        base_dir: An path object indicating the root directory of the project.
+
+        screens: List of screens
+    
+    Methods:
+        add_screen: Adds screen to the screen list
+
+        show: Calls the show method of a screen.
+
+    '''
+
     def __init__(self, base_dir):
         self.base_dir = base_dir
         self.screens = [] # items: {'name': str (unique), 'screen': ptr2screen_obj}
 
     def add_screen(self, screen):
+        '''Adds screen to the screen list.
+        
+        Arguments:
+            screen: A screen object
+        
+        Returns:
+            None
+
+        '''
         self.screens.append(screen)
 
     def show(self, options=None, event=None):
+        '''Call the show method a the specified screen.
+
+        Arguments:
+            options: A dictionary of type \
+                {'screen_name': string, 'file_name':a string specifying article}
+
+            event: An event, automatically passed by the invoked function
+        '''
+
         screen_to_show = None 
         for screen in self.screens:
             if screen['name']==options.get('screen_name'):
